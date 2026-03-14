509e36e525ab26c5a5fdd4eddb2ae35e17c07391
Girish Chapekar <girishchapekar@gmail.com>
2026-03-14 14:41:17 +0530

    updated the requirement.txt file

--- backend/app/db/mongodb.py
+++ backend/app/db/mongodb.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
 from datetime import datetime
+from bson import ObjectId
 
 MONGO_URI = "mongodb://localhost:27017"
 
@@ -13,22 +14,25 @@ posts_collection = db["posts"]
 def get_posts_collection():
     return posts_collection
 
+
 def insert_post(post_data):
     post_data["created_at"] = datetime.utcnow()
     result = posts_collection.insert_one(post_data)
     return str(result.inserted_id)
 
+
 def get_all_posts():
+    posts = []
     for post in posts_collection.find():
         post["_id"] = str(post["_id"])
-        yield post
+        posts.append(post)
+    return posts
 
-from bson import ObjectId
 
 def get_post_by_id(post_id):
     post = posts_collection.find_one({"_id": ObjectId(post_id)})
-    
+
     if post:
         post["_id"] = str(post["_id"])
-        
+
     return post
